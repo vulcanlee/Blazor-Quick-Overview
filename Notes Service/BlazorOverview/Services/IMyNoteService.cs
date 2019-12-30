f88d4e9be553ac953f8a413cbe071958c13763b8
李進興 Vulcan <vulcan@miniasp.com>
2019-12-30 09:28:45 +0800

    建立記事服務並且使用相依性注入服務

---
+++ Notes Service/BlazorOverview/Services/IMyNoteService.cs
@@ -0,0 +1,18 @@
+﻿using BlazorOverview.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorOverview.Services
+{
+    public interface IMyNoteService
+    {
+        // 建立一筆新記事紀錄
+        Task CreateAsync(MyNote myNote);
+        // 刪除記事紀錄
+        Task DeleteAsync(MyNote myNote);
+        // 查詢所有記事紀錄
+        Task<List<MyNote>> RetriveAsync();
+        // 修改記事紀錄
+        Task UpdateAsync(MyNote origMyNote, MyNote myNote);
+    }
+}
