157c777e78cb2e8ee01e3335357f07214601c7da
李進興 Vulcan <vulcan@miniasp.com>
2019-12-30 09:29:24 +0800

    使用資料庫來儲存記事服務相關紀錄

---
+++ Notes Service To Database/BlazorOverview/Models/MyNoteDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorOverview.Models
+{
+    public class MyNoteDbContext : DbContext
+    {
+        public MyNoteDbContext(DbContextOptions<MyNoteDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MyNote> MyNotes { get; set; }
+    }
+}
